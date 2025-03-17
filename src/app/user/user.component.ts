edcbf9b10a0c0056476097c4462b34b4c7796981
BluecollarGiant <Mr.isaiahratliff@gmail.com>
2025-03-17 14:31:49 -0500

    deleting task button

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,15 +1,5 @@
 import { Component, Input, Output, EventEmitter, output} from '@angular/core';
-//type User = {
- // id: string;
- // avatar: string;
-  //name: string;
-//};
-//
-interface User {
-  id: string;
-  avatar: string;
-  name: string;
-};
+import {type User } from './user.model';
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -19,6 +9,7 @@ interface User {
 })
 export class UserComponent {
   @Input({required: true}) user!: User;
+  @Input({required: true}) selected!: boolean;
   @Output() select = new EventEmitter();
   get imagePath() {
     return 'assets/users/' + this.user.avatar;
